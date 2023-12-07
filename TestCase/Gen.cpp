6a30116fd693b26f73c0c0b27ef753dbf332c763
mtmattqq <mtmattqq@gmail.com>
2023-12-07 09:54:45 +0800

    Set up

--- TestCase/Gen.cpp
+++ TestCase/Gen.cpp
@@ -1,27 +1,21 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <algorithm>
+#include <random>
+#include <cassert>
 #include "GraphGen.h"
+#include "tower.h"
 using namespace std;
 
 #define ALL(v) v.begin(),v.end()
 using ll=long long;
 
-unsigned seed=chrono::steady_clock::now().time_since_epoch().count();
-mt19937_64 rng=mt19937_64(seed);
-
-long long RandomNumber(long long a,long long b){
-    uniform_int_distribution<long long> dis(a,b);
-    return dis(rng);
-}
-
-long long RandomNumber(long long n){
-    return RandomNumber(1,n);
-}
-
 int solve(int i){
     string file = "./Sol < " + to_string(i) + ".in" + "> " + to_string(i) + ".out";
     
     int exec_status = system(file.c_str());
-    if (exec_status != 0) {
+    if(exec_status != 0) {
         std::cerr << "Execution failed.\n";
         return 1;
     }
@@ -30,27 +24,21 @@ int solve(int i){
 }
 
 void SubTesk1(int a){
-    string fileName=to_string(a);
+    std::string fileName = to_string(a);
 
-    ofstream ques(fileName+".in");
+    std::ofstream ques(fileName + ".in");
 
-    int aa=RandomNumber(10000),b=RandomNumber(10000);
+    const int SHIFT = 17682;
+    random_number_generater rng(a * 10 + SHIFT);
 
-    ques<<aa<<" "<<b<<"\n";
+    int height = 3;
+    // int height = rng(1, 100);
+    tower tow(height, rng);
 
-    cerr<<a<<endl;
-}
+    ques << height << "\n" << tow;
 
-void SubTesk2(int a){
-    string fileName=to_string(a);
 
-    ofstream ques(fileName+".in");
-
-    int aa=RandomNumber(10000),b=RandomNumber(10000);
-
-    ques<<aa<<" "<<b<<"\n";
-
-    cerr<<a<<endl;
+    std::cerr<<a<<endl;
 }
 
 #define REP(i,a,b) for(int i=(a);i<=(b);++i)
@@ -58,25 +46,23 @@ int main(){
     ios::sync_with_stdio(0);cin.tie(0);
     
     int compile_status = system("g++ Sol.cpp -o Sol");
-    if (compile_status != 0) {
+    if(compile_status != 0) {
         std::cerr << "Compilation failed.\n";
         return 1;
     }
 
+    const int TEST_CASE = 10;
+
     clock_t startTime=clock();
-    REP(i,1,5){
+    REP(i, 1, TEST_CASE){
         SubTesk1(i);
     }
 
-    REP(i,6,10){
-        SubTesk2(i);
-    }
-
-    REP(i,1,10){
+    REP(i, 1, TEST_CASE){
         solve(i);
     }
 
     clock_t endTime=clock();
 
-    cerr<<double(endTime-startTime)/1000<<"\n";
+    cerr << double(endTime - startTime) / CLOCKS_PER_SEC << "\n";
 }
--- TestCase/Sol.cpp
+++ TestCase/Sol.cpp
@@ -1,12 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include "tower.h"
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
 
-    int n, m;
+    int n;
+    std::cin >> n;
+    tower tow(n);
+    std::cin >> tow;
 
-    cin >> n >> m;
-
-    cout << n + m << endl;
+    std::priority_queue<block> exposed_element;
+    for(position &pos : tow.find_coverd_block()) {
+        exposed_element.push({tow.get(pos), pos});
+    }
 }
