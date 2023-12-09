2c81226529acb8fadb9356b6c58f735dd0a2a6e4
mtmattqq <mtmattqq@gmail.com>
2023-12-09 20:36:15 +0800

    Update random number

--- TestCase/GraphGen.h
+++ TestCase/GraphGen.h
@@ -1,22 +1,10 @@
 #include<bits/stdc++.h>
-using namespace std;
+#include "Rng.h"
 
 #ifndef GRAPH_GEN
 #define GRAPH_GEN
 
-unsigned sssddd=chrono::steady_clock().now().time_since_epoch().count();
-mt19937_64 rrr(sssddd);
-
-using ll=long long;
-
-ll Rand(ll a,ll b){
-    uniform_int_distribution<ll> dis(a,b);
-    return dis(rrr);
-}
-
-ll Rand(ll a){
-    return Rand(1,a);
-}
+uint32_t sd=std::chrono::steady_clock().now().time_since_epoch().count();
 
 struct edge{
     int from,to;
@@ -32,147 +20,153 @@ struct edge{
 };
 
 class GraphGen{
-    private :
-        struct DSU{
-            vector<int> dsu,rk;
+private :
+    struct DSU{
+        std::vector<int> dsu,rk;
 
-            DSU(int n){
-                dsu.resize(n+10);
-                rk.resize(n+10);
-                init();
-            }
-
-            void init(){
-                for(int i=0;i<dsu.size();++i){
-                    dsu[i]=i;
-                }
-            }
-
-            int find(int a){
-                if(dsu[a]==a){
-                    return a;
-                }else{
-                    return dsu[a]=find(dsu[a]);
-                }
-            }
-
-            bool same(int a,int b){
-                return find(a)==find(b);
-            }
-
-            void uni(int a,int b){
-                if(same(a,b)){
-                    return;
-                }
-
-                if(rk[find(a)]==rk[find(b)]){
-                    dsu[find(b)]=find(a);
-                    rk[a]++;
-                }else if(rk[find(a)]>rk[find(b)]){
-                    dsu[find(b)]=find(a);
-                }else{
-                    dsu[find(a)]=find(b);
-                }
-            }
-        };
-    public :
-        static vector<pair<int,int>> GenTree(int n){// nodes 1~n
-            DSU dsu(n);
-            vector<pair<int,int>> result;
-
-            while(result.size()<n-1){
-                int a=Rand(n),b=Rand(n);
-                if(!dsu.same(a,b)){
-                    result.emplace_back(a,b);
-                    dsu.uni(a,b);
-                }
-            }
-
-            return result;
+        DSU(int n){
+            dsu.resize(n+10);
+            rk.resize(n+10);
+            init();
         }
 
-        static vector<pair<int,int>> GenConnectedGraph(int n,int m){// n nodes m edges, m need to bigger than n-1
-            vector<pair<int,int>> result=GenTree(n);
+        void init(){
+            for(int i=0;i<dsu.size();++i){
+                dsu[i]=i;
+            }
+        }
 
-            for(int i=0;i<m-n+1;++i){
-                int a,b;
-                do{
-                    a=Rand(n);
-                    b=Rand(n);
-                }while(a==b);
-                
+        int find(int a){
+            if(dsu[a]==a){
+                return a;
+            }else{
+                return dsu[a]=find(dsu[a]);
+            }
+        }
+
+        bool same(int a,int b){
+            return find(a)==find(b);
+        }
+
+        void uni(int a,int b){
+            if(same(a,b)){
+                return;
+            }
+
+            if(rk[find(a)]==rk[find(b)]){
+                dsu[find(b)]=find(a);
+                rk[a]++;
+            }else if(rk[find(a)]>rk[find(b)]){
+                dsu[find(b)]=find(a);
+            }else{
+                dsu[find(a)]=find(b);
+            }
+        }
+    };
+public :
+    static std::vector<pair<int,int>> GenTree(int n){// nodes 1~n
+        DSU dsu(n);
+        std::vector<pair<int,int>> result;
+        random_number_generater rng(sd);
+
+        while(result.size()<n-1){
+            int a=rng(n),b=rng(n);
+            if(!dsu.same(a,b)){
                 result.emplace_back(a,b);
+                dsu.uni(a,b);
             }
-
-            return result;
-        }
-        
-        static vector<pair<int,int>> GenGraph(int n,int m){// n nodes m edges, m need to bigger than n-1
-            vector<pair<int,int>> result;
-
-            for(int i=0;i<m;++i){
-                int a,b;
-                do{
-                    a=Rand(n);
-                    b=Rand(n);
-                }while(a==b);
-                
-                result.emplace_back(a,b);
-            }
-
-            return result;
         }
 
-        static vector<edge> GenTree(int n,int k){// nodes 1~n, weight in 1~k
-            DSU dsu(n);
-            vector<edge> result;
+        return result;
+    }
 
-            while(result.size()<n-1){
-                int a=Rand(n),b=Rand(n),c=Rand(k);
-                if(!dsu.same(a,b)){
-                    result.emplace_back(a,b,c);
-                    dsu.uni(a,b);
-                }
-            }
+    static std::vector<pair<int,int>> GenConnectedGraph(int n,int m){// n nodes m edges, m need to bigger than n-1
+        std::vector<pair<int,int>> result=GenTree(n);
+        random_number_generater rng(sd);
 
-            return result;
+        for(int i=0;i<m-n+1;++i){
+            int a,b;
+            do{
+                a=rng(n);
+                b=rng(n);
+            }while(a==b);
+            
+            result.emplace_back(a,b);
         }
-        
-        static vector<edge> GenConnectedGraph(int n,int m,int k){// n nodes m edges, m need to bigger than n-1
-            vector<edge> result=GenTree(n,k);
 
-            for(int i=0;i<m-n+1;++i){
-                int a,b;
-                do{
-                    a=Rand(n);
-                    b=Rand(n);
-                }while(a==b);
-                
-                int c=Rand(k);
-                
+        return result;
+    }
+    
+    static std::vector<pair<int,int>> GenGraph(int n,int m){// n nodes m edges, m need to bigger than n-1
+        std::vector<pair<int,int>> result;
+        random_number_generater rng(sd);
+
+        for(int i=0;i<m;++i){
+            int a,b;
+            do{
+                a=rng(n);
+                b=rng(n);
+            }while(a==b);
+            
+            result.emplace_back(a,b);
+        }
+
+        return result;
+    }
+
+    static std::vector<edge> GenTree(int n,int k){// nodes 1~n, weight in 1~k
+        DSU dsu(n);
+        std::vector<edge> result;
+        random_number_generater rng(sd);
+
+        while(result.size()<n-1){
+            int a=rng(n),b=rng(n),c=rng(k);
+            if(!dsu.same(a,b)){
                 result.emplace_back(a,b,c);
+                dsu.uni(a,b);
             }
-
-            return result;
         }
-        
-        static vector<edge> GenGraph(int n,int m,int k){// n nodes m edges, m need to bigger than n-1
-            vector<edge> result;
 
-            for(int i=0;i<m;++i){
-                int a,b;
-                do{
-                    a=Rand(n);
-                    b=Rand(n);
-                }while(a==b);
-                
-                int c=Rand(k);
-                
-                result.emplace_back(a,b,c);
-            }
+        return result;
+    }
+    
+    static std::vector<edge> GenConnectedGraph(int n,int m,int k){// n nodes m edges, m need to bigger than n-1
+        std::vector<edge> result=GenTree(n,k);
+        random_number_generater rng(sd);
 
-            return result;
+        for(int i=0;i<m-n+1;++i){
+            int a,b;
+            do{
+                a=rng(n);
+                b=rng(n);
+            }while(a==b);
+            
+            int c=rng(k);
+            
+            result.emplace_back(a,b,c);
         }
+
+        return result;
+    }
+    
+    static std::vector<edge> GenGraph(int n,int m,int k){// n nodes m edges, m need to bigger than n-1
+        std::vector<edge> result;
+        random_number_generater rng(sd);
+
+        for(int i=0;i<m;++i){
+            int a,b;
+            do{
+                a=rng(n);
+                b=rng(n);
+            }while(a==b);
+            
+            int c=rng(k);
+            
+            result.emplace_back(a,b,c);
+        }
+
+        return result;
+    }
 };
 
 #endif
---
+++ TestCase/Rng.h
@@ -0,0 +1,35 @@
+#ifndef RNG_H
+#define RNG_H
+
+#include <random>
+#include <cstdint>
+
+class random_number_generater {
+private :
+    mutable uint32_t seed;
+    std::mt19937_64 rng;
+
+    int64_t rand_int(int64_t a,int64_t b){
+        std::uniform_int_distribution<int64_t> dis(a,b);
+        return dis(rng);
+    }
+
+    int64_t rand_int(int64_t n){
+        return rand_int(1,n);
+    }
+public :
+    random_number_generater(uint32_t _seed) {
+        seed = _seed;
+        rng = std::mt19937_64(seed);
+    }
+
+    int64_t operator()(int64_t a, int64_t b) {
+        return rand_int(a, b);
+    }
+
+    int64_t operator()(int64_t n) {
+        return rand_int(n);
+    }
+};
+
+#endif
