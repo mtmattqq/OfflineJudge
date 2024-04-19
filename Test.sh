b3925ad00085b117580deec6018f22e5fecb3c50
mtmattqq <mtmattqq@gmail.com>
2024-04-19 16:13:31 +0800

    test.sh

--- Test.sh
+++ Test.sh
@@ -4,12 +4,12 @@ echo "Generating the test data."
 
 cd TestCase
 
-g++ -std=c++20 Gen.cpp -o Gen
+g++ -std=c++14 Gen.cpp -o Gen
 ./Gen
 
 cd ..
 
 echo "Compiling Run.cpp"
 
-g++ -std=c++20 Run.cpp -o Run
+g++ -std=c++14 Run.cpp -o Run
 ./Run
